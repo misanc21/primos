5e4e38e702eae68e58d38e4687ca85ac45a155e4
misanc21 <misanc21@gmail.com>
2020-08-22 17:30:51 -0500

    filtrar

--- src/components/TablePrimos.js
+++ src/components/TablePrimos.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { withStyles, makeStyles, useTheme } from '@material-ui/core/styles';
+import { withStyles, makeStyles, useTheme, lighten } from '@material-ui/core/styles';
 import TableContainer from '@material-ui/core/TableContainer';
-import { Table, TableBody, TableCell, TableHead, TableRow, Paper, TablePagination, TableFooter, IconButton } from '@material-ui/core';
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper, TablePagination, TableFooter, IconButton, TableSortLabel, Toolbar, Typography, Tooltip } from '@material-ui/core';
 import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
@@ -10,12 +10,13 @@ import LastPageIcon from '@material-ui/icons/LastPage';
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
-        backgroundColor: theme.palette.common.black,
+        backgroundColor: '#16c1db',
         color: theme.palette.common.white,
+        fontSize: 14
     },
     body: {
         fontSize: 12,
-    },
+    }
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
@@ -26,12 +27,98 @@ const StyledTableRow = withStyles((theme) => ({
     },
 }))(TableRow);
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme)=>({
     table: {
         minWidth: 400,
     },
-});
+    root: {
+        flexShrink: 0,
+        marginLeft: theme.spacing(2.5),
+      },
+      paper: {
+        width: '100%',
+        marginBottom: theme.spacing(2),
+      },
+      visuallyHidden: {
+        border: 0,
+        clip: 'rect(0 0 0 0)',
+        height: 1,
+        margin: -1,
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        top: 20,
+        width: 1,
+      },
+}));
+//filtros
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+  
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+  const headCells = [
+    { id: 'primo', numeric: false, label: 'Primo' },
+    { id: 'fecha', numeric: true, label: 'Fecha' },
+    { id: 'padre', numeric: false, label: 'Padre' },
+  ];
 
+  function EnhancedTableHead(props) {
+    const { classes, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+  
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <StyledTableCell
+              key={headCell.id}
+              align="center"
+              padding="default"
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <span className={classes.visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </span>
+                ) : null}
+              </TableSortLabel>
+            </StyledTableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+
+//Paginacion
 function TablePaginationActions(props) {
     const classes = useStyles();
     const theme = useTheme();
@@ -101,41 +188,52 @@ const TablePrimos = ({ arrayPrimos }) => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+      };
+
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('fecha');
 
     return (
         <TableContainer component={Paper}>
             <Table className={classes.table} aria-label="customized table">
-                <TableHead>
-                    <StyledTableRow>
-                        <StyledTableCell align="center">Nombre</StyledTableCell>
-                        <StyledTableCell align="center">Fecha</StyledTableCell>
-                        <StyledTableCell align="center">Padre</StyledTableCell>
-                    </StyledTableRow>
-                </TableHead>
+                <EnhancedTableHead
+                classes={classes}
+                order={order}
+                orderBy={orderBy}
+                onRequestSort={handleRequestSort}
+                rowCount={rows.length}
+                />
                 <TableBody>
-                    {(rowsPerPage > 0
-                        ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        : rows
-                    ).map((row) => (
-                        <StyledTableRow key={row.id}>
-                            <StyledTableCell component="th" scope="row" align="center">
-                                {row.primo}
-                            </StyledTableCell>
-                            <StyledTableCell  align="center">
-                                {row.fecha}
-                            </StyledTableCell>
-                            <StyledTableCell  align="center">
-                                {row.padre}
-                            </StyledTableCell>
-                        </StyledTableRow>
-                    ))}
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
 
-                    {emptyRows > 0 && (
-                        <StyledTableRow style={{ height: 53 * emptyRows }}>
-                            <StyledTableCell colSpan={6} />
-                        </StyledTableRow>
-                    )}
-                </TableBody>
+                  return (
+                    <StyledTableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.id}
+                    >
+                      <StyledTableCell component="th" id={labelId} scope="row" padding="none" align="center">
+                        {row.primo}
+                      </StyledTableCell>
+                      <StyledTableCell align="center">{row.fecha}</StyledTableCell>
+                      <StyledTableCell align="center">{row.padre}</StyledTableCell>
+                    </StyledTableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <StyledTableRow style={{ height: (false ? 33 : 53) * emptyRows }}>
+                  <StyledTableCell colSpan={6} />
+                </StyledTableRow>
+              )}
+            </TableBody>
                 <TableFooter>
                     <StyledTableRow>
                         <TablePagination
